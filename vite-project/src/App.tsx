c0466273dcb46072a1a24b7df620a4de1e5d46d5
unknown <anhelinapavliuk@gmail.com>
2023-12-03 18:38:32 +0100

    added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,24 @@
-/* import Message from "./Message"; */
+/* import Message from "./Message"; 
 import ListGroup from "./components/List.Group";
+import Alert from "./components/Alert"*/
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 
 function App() {
-  let items = ["New York", "Tokyo", "London", "Paris"];
+ /*  let items = ["New York", "Tokyo", "London", "Paris"];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
-  }
+     {  <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/> }
+  } */
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return( 
     <div>
-      <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button  onClick={() => setAlertVisibility(true)}>My button</Button>
     </div>
   );
 }
